dd6668ba93d3f71804204709a19b931f6445b81f
Cristian <cristian91493@gmail.com>
2023-11-26 21:40:58 -0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+// Menu data structure - array[obj{arr[obj{}]}]
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+
+
+
+let mainEl = document.querySelector('main')
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+
+mainEl.classList.add('flex-ctr')
+
+let topMenuEl = document.getElementById('top-menu')
+
+topMenuEl.style.height = '100%'
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+topMenuEl.classList.add('flex-around')
+
+
+for (let link of menuLinks) {
+  const a = document.createElement('a')
+  a.textContent = link.text // <a>about</a>
+  a.href = link.href // <a href="/about"></a>
+  topMenuEl.appendChild(a)
+}
+
+const subMenuEl = document.getElementById('sub-menu')
+
+// subMenuEl.style.height = "100%"
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+
+subMenuEl.classList.add('flex-around')
+
+subMenuEl.style.position = "absolute"
+
+subMenuEl.style.top = "0"
+
+const topMenuLinks= document.getElementsByTagName("a") //create an array of anchor tags[a,a,a]
+
+let showingSubMenu = false
+
+topMenuEl.addEventListener('click',function(evt){
+  evt.preventDefault()
+  console.dir(evt.target)
+  // if (evt.target.nodeName != "A"){
+  //   return console.log('Not an "A" element')
+  // }
+  evt.target.nodName !== "A"? console.log('Not an "A" element'):console.log(evt.target)
+
+  // evt.target.classList.contains() !== "active"? evt.target.classList.remove('active'):
+  if(evt.target.classList.contains('active')){
+    evt.target.classList.remove('active')
+    showingSubMenu =false
+    subMenuEl.style.top= '0'
+    return
+  }
+  for(let anchor of topMenuLinks){
+    anchor.classList.remove('active')
+  }
+  evt.target.classList.add('active')
+
+const linkObj = menuLinks.find(link => link.text === evt.target.textContent)
+
+  if(linkObj.subLinks){
+    showingSubMenu = true
+  } else{
+    showingSubMenu = false
+  }
+
+  if(showingSubMenu) {
+    buildSubMenu(linkObj.subLinks)
+    subMenuEl.style.top = "100%"
+  }
+  else{
+  subMenuEl.style.top = "0%"}
+
+  function buildSubMenu(){
+    subMenuEl.innerHTML = ""
+    for (let link of linkObj.subLinks) {
+      const a = document.createElement('a')
+      a.textContent = link.text // <a>about</a>
+      a.href = link.href // <a href="/about"></a>
+      subMenuEl.appendChild(a)
+    }
+    
+  }
+  if(evt.target.textContent == 'about'){
+    mainEl.innerHTML = '<h1>ABOUT</>'
+  }
+})
+
+subMenuEl.addEventListener('click',function(evt){
+  evt.preventDefault()
+  console.dir(evt.target)
+  evt.target.nodName !== "A"? console.log('Not an "A" element'):console.log(evt.target)
+  showingSubMenu =false
+  subMenuEl.style.top ='0'
+  for(let anchor of topMenuLinks){
+    anchor.classList.remove('active')
+  }
+
+  let textClicked = evt.target.textContent
+  let finalText = `<h1>${textClicked.toUpperCase()}</h1>`
+  mainEl.innerHTML = finalText
+
+}
+)
